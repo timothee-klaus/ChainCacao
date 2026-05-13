a2f9b5640aa29e4c619c0fc3b163ab44f1243387
Del2pierro <pierrepaulkpatou04@gmail.com>
2026-05-13 22:17:58 +0000

    fix: implémentation de QueryLotsByFarmer dans le chaincode et complétion des tests backend e2e (Certificateur, Transfert, Export)

--- blockchain/tests/chaincode-test/lib/logic/AuditLogic.js
+++ blockchain/tests/chaincode-test/lib/logic/AuditLogic.js
@@ -21,6 +21,38 @@ class AuditLogic {
         const query = { selector: { docType: 'certification', referenceHash: refHash } };
         return await this.ledger.getQueryResultForQueryString(JSON.stringify(query));
     }
+
+    async queryLotsByFarmer(farmerId) {
+        // Étape 1 : Récupérer toutes les parcelles du fermier
+        const parcelleQuery = {
+            selector: { docType: 'parcelle', farmerId: farmerId }
+        };
+        const parcelleIterator = await this.ctx.stub.getQueryResult(JSON.stringify(parcelleQuery));
+        const parcelleIds = [];
+        let res = await parcelleIterator.next();
+        
+        while (!res.done) {
+            if (res.value && res.value.value.toString()) {
+                const parcelle = JSON.parse(res.value.value.toString());
+                parcelleIds.push(parcelle.parcelleId);
+            }
+            res = await parcelleIterator.next();
+        }
+        await parcelleIterator.close();
+
+        if (parcelleIds.length === 0) {
+            return []; // Aucune parcelle trouvée, donc aucun lot
+        }
+
+        // Étape 2 : Récupérer les lots associés à ces parcelles
+        const lotQuery = {
+            selector: {
+                docType: 'lot',
+                parcelleId: { "$in": parcelleIds }
+            }
+        };
+        return await this.ledger.getQueryResultForQueryString(JSON.stringify(lotQuery));
+    }
 }
 
 module.exports = AuditLogic;
